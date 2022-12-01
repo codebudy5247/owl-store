73dd7f0824636a59a2fa248836e12483579bdc2c
codebudy5247 <a.shekhar5247@gmail.com>
2022-12-01 05:23:20 -0800

    add bulk card && update routing

--- src/guard/Seller.tsx
+++ src/guard/Seller.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
+import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const Seller = () => {
-    const auth = localStorage.getItem("authToken");
-    const userRole:any = localStorage.getItem("userRole")
-  return auth && userRole === "ROLE_SELLER" ? <Outlet /> : <Navigate to="/register-seller" />;
-}
+  const auth = localStorage.getItem("authToken");
+  const userRole: any = localStorage.getItem("userRole");
+  return auth && userRole === "ROLE_SELLER" ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/register-seller" />
+  );
+};
 
-export default Seller
+export default Seller;
--- src/pages/merchant/BulkAddCards.tsx
+++ src/pages/merchant/BulkAddCards.tsx
@@ -1,24 +1,42 @@
-import { Stack, TextField, Typography, Box } from "@mui/material";
+import {
+  Stack,
+  TextField,
+  Typography,
+  Box,
+  Container,
+  Table,
+  TableRow,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TablePagination,
+  TableHead,
+  TableSortLabel,
+} from "@mui/material";
 import React, { useState, useRef } from "react";
 import { styled } from "@mui/material/styles";
 import Button, { ButtonProps } from "@mui/material/Button";
 import CurrencyExchangeIcon from "@mui/icons-material/CurrencyExchange";
-import FileDownloadIcon from '@mui/icons-material/FileDownload';
-import Modal from '@mui/material/Modal';
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
+import Modal from "@mui/material/Modal";
+import { Icon } from "@iconify/react";
+import moment from "moment";
+import { AddCardRequestPayload } from "../../services/api";
+import * as Api from "../../services/api";
+import { toast } from "react-toastify";
 var XLSX = require("xlsx");
 
-
 const style = {
-  position: 'absolute' as 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
+  bgcolor: "background.paper",
+  border: "2px solid #000",
   boxShadow: 24,
   p: 4,
-  overflow: 'auto'
+  overflow: "auto",
 };
 
 const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
@@ -29,13 +47,28 @@ const ColorButton = styled(Button)<ButtonProps>(({ theme }) => ({
     // color: "white",
   },
 }));
+const TABLE_HEAD = [
+  { id: "bin", label: "Bin", alignRight: false },
+  { id: "base", label: "Base", alignRight: false },
+  { id: "zip", label: "Zip", alignRight: false },
+  { id: "city", label: "City", alignRight: true },
+  { id: "state", label: "State", alignRight: true },
+  { id: "country", label: "Country", alignRight: true },
+  { id: "price", label: "Price", alignRight: true },
+];
+const displayIcon = (type: any) => {
+  if (type === "master")
+    return <Icon icon="logos:mastercard" height={40} width={40} />;
+  if (type === "visa") return <Icon icon="logos:visa" height={40} width={40} />;
+  if (type === "discover")
+    return <Icon icon="logos:discover" height={40} width={40} />;
+};
+
 const BulkAddCards = () => {
   const inputFileRef = useRef<HTMLInputElement>(null);
-  const [open, setOpen] = React.useState(false);
-  const [xlsxData,setXlsxData] = useState<any>()
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+  const [xlsxData, setXlsxData] = useState<any>();
 
+  // console.log("xlsxData______", xlsxData);
 
   const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
@@ -47,8 +80,7 @@ const BulkAddCards = () => {
         const sheetName = workbook.SheetNames[0];
         const worksheet = workbook.Sheets[sheetName];
         const json = XLSX.utils.sheet_to_json(worksheet);
-        setXlsxData(json)
-        handleOpen()
+        setXlsxData(json);
       };
       reader.readAsArrayBuffer(e.target.files[0]);
     }
@@ -56,6 +88,40 @@ const BulkAddCards = () => {
 
   const handleInputFileRefClick = () => {
     inputFileRef.current?.click();
+  };
+
+  const handleSubmit = async () => {
+    for (const item of xlsxData) {
+      const payloadObj: AddCardRequestPayload = {
+        street: item.street,
+        country: item.country,
+        state: item.state,
+        city: item.city,
+        zip: item.zip,
+        mobile: item.phoneNo,
+        cardNumber: item.cardNumber,
+        expiryDate: item.expiryDate,
+        cvv: item.cvv,
+        socialSecurityNumber: item.socialSecurityNumber,
+        drivingLicenceNumber: item.drivingLicenceNumber,
+        level: item.level,
+        class: item.class,
+        price: item.price,
+        bankName: item.bankName,
+        type: item.type,
+      };
+
+      // call add product api
+      const [err, res] = await Api.createCard(payloadObj);
+      if (err) {
+        toast.error("Something went wrong!", {
+          position: toast.POSITION.TOP_RIGHT,
+        });
+      }
+      if (res) {
+        
+      }
+    }
   };
   return (
     <>
@@ -76,7 +142,106 @@ const BulkAddCards = () => {
           style={{ display: "none" }}
         />
       </Box>
-      <Stack spacing={2} sx={{ mt: 2 }}>
+
+      {xlsxData?.length === 0 || xlsxData === undefined ? (
+        <></>
+      ) : (
+        <Container maxWidth="lg" sx={{ mt: 2, mb: 2 }}>
+          <TableContainer sx={{ minWidth: 500 }}>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  {TABLE_HEAD?.map((headCell: any) => (
+                    <TableCell key={headCell.id}>
+                      <TableSortLabel hideSortIcon>
+                        {headCell.label}
+                      </TableSortLabel>
+                    </TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {xlsxData?.length >0 && xlsxData?.map((card: any) => (
+                  <>
+                    <TableRow key={card?._id}>
+                      <TableCell sx={{ display: "flex" }}>
+                        {displayIcon(card?.type)}
+                        <Typography
+                          variant="subtitle2"
+                          noWrap
+                          sx={{ ml: 1, mt: 1 }}
+                        >
+                          {card?.cardNumber?.slice(0, 6)}
+                        </Typography>
+                      </TableCell>
+                      <TableCell sx={{ p: 2 }}>
+                        <Box
+                          sx={{
+                            backgroundColor: "#FDE7EF",
+                            p: 1,
+                            textAlign: "center",
+                          }}
+                        >
+                          <Typography variant="subtitle2" noWrap>
+                            {moment(card?.base).format("MMMM YY")}
+                          </Typography>
+                        </Box>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle2" noWrap>
+                          {card?.zip}
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle2" noWrap>
+                          {card?.city}
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle2" noWrap>
+                          {card?.state}
+                        </Typography>
+                      </TableCell>
+                      <TableCell>
+                        <img
+                          loading="lazy"
+                          width="50"
+                          height="25"
+                          src={`https://countryflagsapi.com/png/${card?.country?.toLowerCase()}`}
+                          alt=""
+                        />
+                      </TableCell>
+                      <TableCell>
+                        <Typography variant="subtitle2" noWrap>
+                          {card?.price}
+                        </Typography>
+                      </TableCell>
+                    </TableRow>
+                  </>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+
+          <Stack
+            direction={{ xs: "column", sm: "row" }}
+            spacing={5}
+            sx={{ display: "flex", justifyContent: "flex-end" }}
+          >
+            <ColorButton
+              variant="contained"
+              startIcon={<CurrencyExchangeIcon />}
+              sx={{ mt: 3 }}
+              onClick={handleSubmit}
+            >
+              Submit
+            </ColorButton>
+            {/*  */}
+          </Stack>
+        </Container>
+      )}
+
+      {/* <Stack spacing={2} sx={{ mt: 2 }}>
         <TextField
           fullWidth
           id="filled-multiline-static"
@@ -93,30 +258,7 @@ const BulkAddCards = () => {
           // defaultValue="Enter Amount"
           variant="outlined"
         />
-      </Stack>
-      <Stack direction={{ xs: "column", sm: "row" }} spacing={5}>
-        <ColorButton
-          variant="contained"
-          startIcon={<CurrencyExchangeIcon />}
-          sx={{ mt: 3 }}
-        >
-          Submit
-        </ColorButton>
-        {/*  */}
-      </Stack>
-
-      <div>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <p>{JSON.stringify(xlsxData)}</p>
-        </Box>
-      </Modal>
-    </div>
+      </Stack> */}
     </>
   );
 };
